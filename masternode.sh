cc1f765b3c76926b600b7e55bb12b3656ab2f5e3
Velescore <42907171+Velescore@users.noreply.github.com>
2018-11-27 22:18:33 +0100

    masternode install script

---
+++ masternode.sh
@@ -0,0 +1,250 @@
+#!/bin/bash
+
+TMP_FOLDER=$(mktemp -d)
+USER='veles'
+CONFIG_FILE='veles.conf'
+CONFIGFOLDER='/home/veles/.veles'
+COIN_DAEMON='velesd'
+COIN_CLI='veles-cli'
+COIN_PATH='/usr/local/bin/'
+COIN_TGZ='https://github.com/Velescore/Veles-0.16/releases/download/v16/linuxunknown.tar.gz'
+COIN_ZIP=$(echo $COIN_TGZ | awk -F'/' '{print $NF}')
+COIN_NAME='veles'
+COIN_PORT=21337
+RPC_PORT=21338
+
+NODEIP=$(curl -s4 api.ipify.org)
+
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
+
+function download_node() {
+  echo -e "Preparing to download ${GREEN}$COIN_NAME${NC}."
+  cd $TMP_FOLDER >/dev/null 2>&1
+  wget -q $COIN_TGZ
+  compile_error
+  tar xvzf $COIN_ZIP -C $COIN_PATH >/dev/null 2>&1
+  cd - >/dev/null 2>&1
+  rm -rf $TMP_FOLDER >/dev/null 2>&1
+  clear
+}
+
+function create_user() {
+  echo -e "Setting up new user for masternode.."
+  # our new mnode unpriv user acc is added
+  if id "$USER" >/dev/null 2>&1; then
+    echo "* User exists already, do nothing"                       
+   else
+     echo "* Adding new system user ${user}"
+     useradd -m $USER
+     su - $USER -c "cd - >/dev/null 2>&1"
+     su - $USER -c "mkdir $CONFIGFOLDER >/dev/null 2>&1"	 
+     su - $USER -c "touch $CONFIGFOLDER/$CONFIG_FILE >/dev/null 2>&1"
+	 echo "$USER	ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+  fi
+}
+
+function check_ufw() {
+  #check if is installed ufw if yes setup
+  if [ -f "/sbin/ufw" ] && ufw status | grep -wq 'active'; then 
+    echo "Ufw is active and will be set";
+    setup_ufw
+  fi
+}
+
+function setup_ufw() {
+  #will setup ufw
+  echo -e "Installing and setting up firewall to allow ingress on port ${GREEN}$COIN_PORT${NC}"
+  ufw allow $COIN_PORT/tcp comment "$COIN_NAME MN port" >/dev/null 2>&1
+  ufw allow ssh comment "SSH" >/dev/null 2>&1
+  ufw limit ssh/tcp >/dev/null 2>&1
+  ufw default allow outgoing >/dev/null 2>&1
+  echo "Ufw is configured" | ufw enable >/dev/null 2>&1
+}
+ 
+function configure_systemd() {
+  cat << EOF > /etc/systemd/system/$COIN_NAME.service
+[Unit]
+Description=$COIN_NAME service
+After=network.target
+[Service]
+User=$USER
+Group=$USER
+Type=forking
+#PIDFile=$CONFIGFOLDER/$COIN_NAME.pid
+ExecStart=$COIN_PATH$COIN_DAEMON -daemon -conf=$CONFIGFOLDER/$CONFIG_FILE -datadir=$CONFIGFOLDER
+ExecStop=-$COIN_PATH$COIN_CLI -conf=$CONFIGFOLDER/$CONFIG_FILE -datadir=$CONFIGFOLDER stop
+Restart=always
+PrivateTmp=true
+TimeoutStopSec=60s
+TimeoutStartSec=10s
+StartLimitInterval=120s
+StartLimitBurst=5
+[Install]
+WantedBy=multi-user.target
+EOF
+   
+  systemctl daemon-reload
+  sleep 3
+  systemctl start $COIN_NAME.service
+  systemctl enable $COIN_NAME.service >/dev/null 2>&1
+  su $USER;cd $CONFIGFOLDER
+  
+  if [[ -z "$(ps axo cmd:100 | egrep $COIN_DAEMON)" ]]; then
+    echo -e "${RED}$COIN_NAME is not running${NC}, please investigate. You should start by running the following commands as root:"
+    echo -e "${GREEN}systemctl start${NC} $COIN_NAME.service"
+    echo -e "systemctl status $COIN_NAME.service"
+    echo -e "less /var/log/syslog${NC}"
+    exit 1
+  fi
+}
+
+
+function create_config() {
+  RPCUSER=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w10 | head -n1)
+  RPCPASSWORD=$(tr -cd '[:alnum:]' < /dev/urandom | fold -w22 | head -n1)
+  cat << EOF > $CONFIGFOLDER/$CONFIG_FILE
+rpcuser=$RPCUSER
+rpcpassword=$RPCPASSWORD
+rpcport=$RPC_PORT
+rpcallowip=127.0.0.1
+listen=1
+server=1
+daemon=1
+port=$COIN_PORT
+EOF
+}
+
+function create_key() {
+  echo -e "Enter your ${RED}$COIN_NAME Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
+  read -e COINKEY
+  if [[ -z "$COINKEY" ]]; then
+  $COIN_PATH$COIN_DAEMON -daemon
+  sleep 30
+  if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
+    echo -e "${RED}$COIN_NAME server couldn not start. Check /var/log/syslog for errors.{$NC}"
+    exit 1
+  fi
+  COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
+  if [ "$?" -gt "0" ];then
+    echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the Private Key${NC}"
+    sleep 30
+    COINKEY=$($COIN_PATH$COIN_CLI masternode genkey)
+  fi
+  $COIN_PATH$COIN_CLI stop
+fi
+clear
+}
+
+function update_config() {
+  sed -i 's/daemon=1/daemon=0/' $CONFIGFOLDER/$CONFIG_FILE
+  cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
+logintimestamps=1
+maxconnections=256
+#bind=$NODEIP
+txindex=1
+listenonion=0
+masternode=1
+#externalip=$NODEIP:$COIN_PORT
+masternodeaddr=$NODEIP:$COIN_PORT
+masternodeprivkey=$COINKEY
+EOF
+}
+
+
+
+
+
+function get_ip() {
+  declare -a NODE_IPS
+  for ips in $(netstat -i | awk '!/Kernel|Iface|lo/ {print $1," "}')
+  do
+    NODE_IPS+=($(curl --interface $ips --connect-timeout 2 -s4 api.ipify.org))
+  done
+
+  if [ ${#NODE_IPS[@]} -gt 1 ]
+    then
+      echo -e "${GREEN}More than one IP. Please type 0 to use the first IP, 1 for the second and so on...${NC}"
+      INDEX=0
+      for ip in "${NODE_IPS[@]}"
+      do
+        echo ${INDEX} $ip
+        let INDEX=${INDEX}+1
+      done
+      read -e choose_ip
+      NODEIP=${NODE_IPS[$choose_ip]}
+  else
+    NODEIP=${NODE_IPS[0]}
+  fi
+}
+
+
+function compile_error() {
+if [ "$?" -gt "0" ];
+ then
+  echo -e "${RED}Failed to compile $COIN_NAME. Please investigate.${NC}"
+  exit 1
+fi
+}
+
+
+function checks() {
+if [ -n "$(pidof $COIN_DAEMON)" ] || [ -e "$COIN_DAEMOM" ] ; then
+  echo -e "${RED}$COIN_NAME is already installed.${NC}"
+  exit 1
+fi
+}
+
+
+function important_information() {
+ echo -e "==================================================================================================================="
+ echo -e "${RED} _   __ ____ __    ____ ____   __  ___ ___    ____ ______ ____ ___   _  __ ____   ___   ____ ${NC}  "
+ echo -e "${RED}| | / // __// /   / __// __/  /  |/  // _ |  / __//_  __// __// _ \ / |/ // __ \ / _ \ / __/ ${NC} "
+ echo -e "${RED}| |/ // _/ / /__ / _/ _\ \   / /|_/ // __ | _\ \   / /  / _/ / , _//    // /_/ // // // _/  ${NC} "
+ echo -e "${RED}|___//___//____//___//___/  /_/  /_//_/ |_|/___/  /_/  /___//_/|_|/_/|_/ \____//____//___/ ${NC} "
+ echo -e "                                                                                            "
+ echo -e "==================================================================================================================="
+ echo -e "$COIN_NAME Masternode is up and running listening on port ${GREEN}$COIN_PORT${NC}."
+ echo -e "Configuration file is: ${GREEN}$CONFIGFOLDER/$CONFIG_FILE${NC}"
+ echo -e "Start: ${GREEN}systemctl start $COIN_NAME.service${NC}"
+ echo -e "Stop: ${GREEN}systemctl stop $COIN_NAME.service${NC}"
+ echo -e "VPS_IP:PORT ${GREEN}$NODEIP:$COIN_PORT${NC}"
+ echo -e "MASTERNODE PRIVATEKEY is: ${GREEN}$COINKEY${NC}"
+ echo -e "Please check ${GREEN}$COIN_NAME${NC} daemon is running with the following command: ${GREEN}systemctl status $COIN_NAME.service${NC}"
+ echo -e "Use ${GREEN}$COIN_CLI masternode status${NC} to check your MN."
+ echo -e "For help join discord ${RED}https://discord.gg/P528fGg ..."
+ if [[ -n $SENTINEL_REPO  ]]; then
+  echo -e "${GREEN}Sentinel${NC} is installed in ${RED}$CONFIGFOLDER/sentinel${NC}"
+  echo -e "Sentinel logs is: ${GREEN}$CONFIGFOLDER/sentinel.log${NC}"
+ fi
+ echo -e "==================================================================================================================="
+}
+
+function setup_node() {
+  echo -e "Wait for a while install process ${GREEN}start."
+  create_user
+  get_ip
+  check_ufw
+  create_config
+  create_key
+  update_config
+  important_information
+  configure_systemd
+ }
+
+
+
+##### Main #####
+clear
+
+clear
+echo "Checking system..."
+checks
+echo "Downlading node ..."
+download_node
+echo "Setting up node ..."
+setup_node
+echo "Congratulations, installation success!"
