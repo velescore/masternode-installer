d45e9036f56ee2e4c78c66c5cf36d068b132d8f2
AltcoinBaggins <marian@priv.profiweb.biz>
2019-02-09 09:13:43 +0100

    Non-interactive mode for testing purposes

--- masternode.sh
+++ masternode.sh
@@ -126,8 +126,10 @@ EOF
 }
 
 function create_key() {
-  echo -e "Enter your ${RED}$COIN_NAME Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
-  if [ $0 != '--nonint' ]; then # skip reading in non-interactive mode
+  if [ $1 == '--nonint' ]; then # skip reading in non-interactive mode
+    echo "[non interactive mode] Generating new masternode private key ..."
+  else
+    echo -e "Enter your ${RED}$COIN_NAME Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
     read -e COINKEY
   fi
   if [[ -z "$COINKEY" ]]; then
