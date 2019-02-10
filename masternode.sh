5655267cdcf88b89a82e05bfe79ae5349787bd1b
AltcoinBaggins <marian@priv.profiweb.biz>
2019-02-10 12:50:31 +0100

    Enhaced error message when service doesn't start

--- masternode.sh
+++ masternode.sh
@@ -14,8 +14,8 @@ USER='veles'
 CONFIG_FILENAME='veles.conf'
 DATADIR_PATH='/home/veles/.veles'
 COIN_DAEMON='velesd'
-COIN_CLI='veles-cli'
-INSTALL_PATH='/usr/local/bin/'
+${COIN_CLI}='veles-cli'
+INSTALL_PATH='/usr/local/bin'
 COIN_TGZ_URL='https://github.com/Velescore/Veles/releases/download/v0.17.0.21/velesLinux.tar.gz'
 COIN_NAME='Veles Core'
 COIN_NAME_SHORT='veles'
@@ -53,6 +53,24 @@ function perr() {
   exit 1
 }
 
+function check_system() {
+  echo -en "${ST} Looking for previous installation ...                                 "
+  if [ -n "$(pidof $COIN_DAEMON)" ] || [ -e "${INSTALL_PATH}/${COIN_DAEMON}" ] ; then
+    perr "$COIN_NAME is already installed."
+  fi
+  pok
+}
+
+function check_ufw() {
+  echo -en "${ST} Checking whether UFW firewall is present ... "
+  if [ -f "/sbin/ufw" ] && ufw status | grep -wq 'active'; then 
+    echo "yes"
+    setup_ufw
+  else
+    echo "no"
+  fi
+}
+
 function download_node() {
   echo -en "${ST} Downloading installation archive ...                                  "
   cd $TEMP_PATH >/dev/null 2>&1 || perr "Cannot change to the temporary directory: $TEMP_PATH"
@@ -60,7 +78,7 @@ function download_node() {
   
   archive_name=$(echo $COIN_TGZ_URL | awk -F'/' '{print $NF}')
 
-  tar xvzf $archive_name -C $INSTALL_PATH >/dev/null 2>&1 || "Failed to extract installation archive $archive_name to $INSTALL_PATH"
+  tar xvzf $archive_name -C ${$INSTALL_PATH}/ >/dev/null 2>&1 || "Failed to extract installation archive $archive_name to ${$INSTALL_PATH}"
   pok
   rm -rf $TEMP_PATH >/dev/null 2>&1 || echo -e "\n{$BRED} !   ${YELLOW}Warning: Failed to remove temporary directory: ${TEMP_PATH}${NC}\n"
 }
@@ -81,19 +99,9 @@ function create_user() {
   fi
 }
 
-function check_ufw() {
-  echo -en "${ST} Checking whether UFW firewall is present ... "
-  if [ -f "/sbin/ufw" ] && ufw status | grep -wq 'active'; then 
-    echo "yes"
-    setup_ufw
-  else
-    echo "no"
-  fi
-}
-
 function setup_ufw() {
   echo -en "${ST}   Enabling inbound traffic on TCP port ${BYELLOW}${COIN_PORT}${NC} ...                      "
-  ufw allow $COIN_PORT/tcp comment "$COIN_NAME_SHORT MN port" >/dev/null 2>&1 || perr "Failed to set-up UFW (ufw allow $COIN_PORT/tcp)"
+  ufw allow $COIN_PORT/tcp comment "${COIN_NAME_SHORT} MN port" >/dev/null 2>&1 || perr "Failed to set-up UFW (ufw allow $COIN_PORT/tcp)"
   ufw allow ssh comment "SSH" >/dev/null 2>&1 || perr "Failed to set-up UFW (ufw allow $COIN_PORT/tcp)"
   ufw limit ssh/tcp >/dev/null 2>&1 || perr "Failed to set-up UFW (ufw allow $COIN_PORT/tcp)"
   ufw default allow outgoing >/dev/null 2>&1 || perr "Failed to set-up UFW (ufw allow $COIN_PORT/tcp)"
@@ -103,17 +111,17 @@ function setup_ufw() {
  
 function configure_systemd() {
   echo -en "${ST} Creating systemd service ${BYELLOW}${COIN_NAME_SHORT}${NC} ...                                    "
-  cat << EOF > /etc/systemd/system/$COIN_NAME_SHORT.service && pok || perr
+  cat << EOF > /etc/systemd/system/${COIN_NAME_SHORT}.service && pok || perr
 [Unit]
-Description=$COIN_NAME_SHORT service
+Description=${COIN_NAME_SHORT} service
 After=network.target
 [Service]
 User=$USER
 Group=$USER
 Type=forking
-#PIDFile=$DATADIR_PATH/$COIN_NAME_SHORT.pid
-ExecStart=$INSTALL_PATH$COIN_DAEMON -daemon -conf=$DATADIR_PATH/$CONFIG_FILENAME -datadir=$DATADIR_PATH
-ExecStop=-$INSTALL_PATH$COIN_CLI -conf=$DATADIR_PATH/$CONFIG_FILENAME -datadir=$DATADIR_PATH stop
+#PIDFile=$DATADIR_PATH/${COIN_NAME_SHORT}.pid
+ExecStart=${$INSTALL_PATH}/$COIN_DAEMON -daemon -conf=$DATADIR_PATH/$CONFIG_FILENAME -datadir=$DATADIR_PATH
+ExecStop=-${$INSTALL_PATH}/${COIN_CLI} -conf=$DATADIR_PATH/$CONFIG_FILENAME -datadir=$DATADIR_PATH stop
 Restart=always
 PrivateTmp=true
 TimeoutStopSec=60s
@@ -126,18 +134,31 @@ EOF
   echo -en "${ST} Reloading systemctl ...                                               "
   systemctl daemon-reload && pok || perr "Failed to reload systemd daemon (systemctl daemon-reload)"
   echo -en "${ST} Setting up the service to auto-start on system boot ...               "
-  systemctl enable $COIN_NAME_SHORT.service >/dev/null 2>&1 && pok || perr "Failed to enable systemd servie ${COIN_NAME_SHORT}.service"
+  systemctl enable ${COIN_NAME_SHORT}.service >/dev/null 2>&1 && pok || perr "Failed to enable systemd servie ${COIN_NAME_SHORT}.service"
   #u $USER;cd $DATADIR_PATH
 }
 
 function start_systemd_service() {
   echo -en "${ST} Starting ${BYELLOW}${COIN_NAME_SHORT}${NC} service ...                                            "
-  systemctl start "${COIN_NAME_SHORT}.service" && pok || perr "Failed to start systemd service ${COIN_NAME_SHORT}.service"
+  systemctl start "${COIN_NAME_SHORT}.service"
+  sleep 1   # just in case
+  if [ -n "$(pidof ${COIN_DAEMON})" ]; then
+    pok
+  else
+    perr "Daemon ${COIN_DAEMON} is not running, please investigate. You can start by 
+running following commands as root: ${BYELLOW}
+systemctl start ${COIN_NAME_SHORT}.service
+systemctl status ${COIN_NAME_SHORT}.service
+cat ${DATADIR_PATH}/debug.log
+${NC}"
+  fi
+          
+  && pok || perr "Failed to start systemd service ${COIN_NAME_SHORT}.service"
 
   if [[ -z "$(ps axo cmd:100 | egrep $COIN_DAEMON)" ]]; then
-    echo -e "${RED}$COIN_NAME_SHORT is not running${NC}, please investigate. You should start by running the following commands as root:"
-    echo -e "${GREEN}systemctl start${NC} $COIN_NAME_SHORT.service"
-    echo -e "systemctl status $COIN_NAME_SHORT.service"
+    echo -e "${RED}${COIN_NAME_SHORT} is not running${NC}, please investigate. You should start by running the following commands as root:"
+    echo -e "${GREEN}systemctl start${NC} ${COIN_NAME_SHORT}.service"
+    echo -e "systemctl status ${COIN_NAME_SHORT}.service"
     echo -e "less /var/log/syslog${NC}"
     exit 1
   fi
@@ -161,23 +182,23 @@ EOF
 
 function create_key() {
   if ! [ $ARG1 == '--nonint' ]; then # skip reading in non-interactive mode
-    echo -e "Enter your ${RED}$COIN_NAME_SHORT Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
+    echo -e "Enter your ${RED}${COIN_NAME_SHORT} Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
     read -e COINKEY
   fi
   if [[ -z "$COINKEY" ]]; then
     echo -en "${ST} Generating masternode private key ...                                 "
-    $INSTALL_PATH$COIN_DAEMON -daemon >/dev/null 2>&1
+    ${$INSTALL_PATH}/$COIN_DAEMON -daemon >/dev/null 2>&1
     sleep 30
     if [ -z "$(ps axo cmd:100 | grep $COIN_DAEMON)" ]; then
-      perr "${RED}$COIN_NAME_SHORT server couldn not start. Check /var/log/syslog for errors.${NC}"
+      perr "${RED}${COIN_NAME_SHORT} server couldn not start. Check /var/log/syslog for errors.${NC}"
     fi
-    COINKEY=$($INSTALL_PATH$COIN_CLI masternode genkey)
+    COINKEY=$(${$INSTALL_PATH}/${COIN_CLI} masternode genkey)
     if [ "$?" -gt "0" ];then
       echo -e "${RED}Wallet not fully loaded. Let us wait and try again to generate the Private Key${NC}"
       sleep 30
-      COINKEY=$($INSTALL_PATH$COIN_CLI masternode genkey)
+      COINKEY=$(${$INSTALL_PATH}/${COIN_CLI} masternode genkey)
     fi
-    $INSTALL_PATH$COIN_CLI stop >/dev/null 2>&1
+    ${$INSTALL_PATH}/${COIN_CLI} stop >/dev/null 2>&1
   fi
   pok
 }
@@ -225,22 +246,14 @@ function get_ip() {
   fi
 }
 
-function check_system() {
-  echo -en "${ST} Looking for previous installation ...                                 "
-  if [ -n "$(pidof $COIN_DAEMON)" ] || [ -e "${INSTALL_PATH}${COIN_DAEMON}" ] ; then
-    perr "$COIN_NAME is already installed."
-  fi
-  pok
-}
-
 function print_installed_version() {
   echo -en "${BGREEN}"
-  $INSTALL_PATH$COIN_CLI -version | head -n 1
+  ${$INSTALL_PATH}/${COIN_CLI} -version | head -n 1
   echo -en "${NC}"
 }
 
 function print_logo() {
-  $INSTALL_PATH$COIN_CLI -version | head -n 1 | head -n 5   # Current Veles Core ASCII logo
+  ${$INSTALL_PATH}/${COIN_CLI} -version | head -n 6 | tail -n 5   # Current Veles Core ASCII logo
   echo -e "     __  ___ ___   ____ ______ ____ ___   _  __ ____   ___   ____"
   echo -e "    /  |/  // _ | / __//_  __// __// _ \ / |/ // __ \ / _ \ / __/"
   echo -e "   / /|_/ // __ |_\ \   / /  / _/ / , _//    // /_/ // // // _/  "
@@ -253,12 +266,12 @@ function print_success_screen() {
   print_installed_version
   echo -e "\n$COIN_NAME Masternode is up and running listening on port ${GREEN}$COIN_PORT${NC}."
   echo -e "Configuration file is: ${GREEN}$DATADIR_PATH/$CONFIG_FILENAME${NC}"
-  echo -e "Start: ${GREEN}systemctl start $COIN_NAME_SHORT.service${NC}"
-  echo -e "Stop: ${GREEN}systemctl stop $COIN_NAME_SHORT.service${NC}"
+  echo -e "Start: ${GREEN}systemctl start ${COIN_NAME_SHORT}.service${NC}"
+  echo -e "Stop: ${GREEN}systemctl stop ${COIN_NAME_SHORT}.service${NC}"
   echo -e "VPS_IP:PORT ${GREEN}$NODEIP:$COIN_PORT${NC}"
   echo -e "MASTERNODE PRIVATEKEY is: ${GREEN}$COINKEY${NC}"
-  echo -e "Please check ${GREEN}$COIN_NAME_SHORT${NC} daemon is running with the following command: ${GREEN}systemctl status $COIN_NAME_SHORT.service${NC}"
-  echo -e "Use ${GREEN}$COIN_CLI masternode status${NC} to check your MN."
+  echo -e "Please check ${GREEN}${COIN_NAME_SHORT}${NC} daemon is running with the following command: ${GREEN}systemctl status ${COIN_NAME_SHORT}.service${NC}"
+  echo -e "Use ${GREEN}${COIN_CLI} masternode status${NC} to check your MN."
   echo -e "For help join discord ${RED}https://discord.gg/P528fGg${NC} ..."
   if [[ -n $SENTINEL_REPO  ]]; then
   echo -e "${GREEN}Sentinel${NC} is installed in ${RED}$DATADIR_PATH/sentinel${NC}"
